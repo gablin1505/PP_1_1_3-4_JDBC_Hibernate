d47a5a2c8cbeacadf2e85589be884c070d814af3
admin <gablin233@mail.ru>
2025-01-22 01:17:19 +0300

    done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,17 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl(new UserDaoJDBCImpl());
+        boolean exit = false;
+        userService.createUsersTable();
+        userService.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.removeUserById(1);
+        userService.getAllUsers();
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,12 +1,10 @@
 package jm.task.core.jdbc.model;
 
 import javax.persistence.Column;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table
 public class User {
-    @Id
     private Long id;
 
     @Column
@@ -58,5 +56,15 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
